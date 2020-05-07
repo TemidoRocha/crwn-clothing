f4a935ce94d6485295a72380ad02a379bbf759bf
TemidoRocha <manueltemidorocha@gmail.com>
2020-05-07 15:02:35 +0100

    implement section data reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -8,6 +8,7 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 
 //the JSON obj that represents the probable config of the obj we need
 const persistConfig = {
@@ -19,6 +20,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
