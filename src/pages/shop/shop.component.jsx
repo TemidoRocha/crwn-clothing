81faea124ca75f2dc3a9efc21319d87eee12418a
TemidoRocha <manueltemidorocha@gmail.com>
2020-05-11 17:07:02 +0100

    included with spinner HOC

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,32 +2,63 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import CollectionsOverview from './../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
-
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
 import updateCollections from '../../redux/shop/shop.actions';
+import CollectionsOverview from './../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionsPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
+  state = {
+    loading: true,
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async (snapshot) => {
+    //using firebase library
+    // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async (snapshot) => {
+    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //   updateCollections(collectionsMap);
+    //   this.setState({ loading: false });
+    // });
+
+    //using promisses pattern
+    collectionRef.get().then((snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      this.setState({ loading: false });
     });
+
+    //using fetch just to see how deep nested it is
+    // fetch(
+    //   'https://firestore.googleapis.com/v1/projects/crwn-clothing-udemy/databases/(default)/documents/collections'
+    // )
+    //   .then((response) => response.json())
+    //   .then((collections) => console.log(collections));
   }
 
   render() {
+    const { loading } = this.state;
     const { match } = this.props;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route path={`${match.path}/:categoryId`} component={CollectionPage} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />}
+        />
+        <Route
+          path={`${match.path}/:categoryId`}
+          render={(props) => <CollectionsPageWithSpinner isLoading={loading} {...props} />}
+        />
       </div>
     );
   }
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -5,10 +5,12 @@ const selectShop = (state) => state.shop;
 export const selectCollections = createSelector([selectShop], (shop) => shop.collections);
 
 export const selectCollectionsForPreview = createSelector([selectCollections], (collections) =>
-  Object.values(collections)
+  collections ? Object.values(collections) : []
 );
 
 //Storing list of elements inside na object instead of na array  data normalization (shop.data was changed from array to obj)
 //this way is as fast as looking for the last element as for the beginning, what does,t happen with an array
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], (collections) => collections[collectionUrlParam]);
+  createSelector([selectCollections], (collections) =>
+    collections ? collections[collectionUrlParam] : null
+  );
