55845b359a3ef67a75cf8a24189b8bb365f24042
TemidoRocha <manueltemidorocha@gmail.com>
2020-07-17 16:55:29 +0100

    media queries introduced for mobile responsive at header, home  and shop

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
+
+import { GlobalStyle } from '../src/global.styles';
+
 import { selectCurrentUser } from './redux/user/user.selector';
 import { checkUserSession } from './redux/user/user.actions';
 
@@ -25,7 +26,7 @@ const App = ({ checkUserSession, currentUser }) => {
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
         <Route exact path="/checkout" component={CheckoutPage} />
-        />
+
         <Route
           exact
           path="/signin"
